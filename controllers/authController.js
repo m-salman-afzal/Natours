b336a6785aa3a4bb7fddf39d3e044d64fbd48ed8
muhammadsalmanafzal <m.salmanafzal@hotmail.com>
2022-01-31 21:38:03 +0500

    ADD: Added cookie to send token in it.

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,6 +23,20 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res, req) => {
   const token = signToken(user._id);
+
+  // * create cookie
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV == 'production') {
+    cookieOptions.secure = true;
+  }
+
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({
     status: 'success',
     time: req.reqtime,
